fa967f8949846eafe6c8f745b4ac6844f120d127
Kalomiris Nikos <nikalomiris@gmail.com>
2018-11-12 09:45:53 +0200

    All standard tests passing, 8 pending

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+    num1 + num2
+end
+
+def subtract(num1, num2)
+    num1 - num2
+end
+
+def sum(nums)
+    sum = 0
+    nums.each do |i|
+        sum = sum +i
+    end
+    sum
+end
