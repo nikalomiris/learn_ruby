e1068adb9798f5ad30cc4db8039a7366d1cfe20f
Kalomiris Nikos <nikalomiris@gmail.com>
2018-11-13 20:56:24 +0200

    All test passing

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(input) 
+    input
+end
+
+def shout(input)
+    input.upcase
+end
+
+def repeat(input, times=2)
+    result = ((input + " ")*times).chomp(" ")
+end
+
+def start_of_word(input, chars)
+    result = input[0...chars]
+end
+
+def first_word(sentence)
+    result = sentence.partition(" ").first
+end
+
+def titleize(word)
+    word_splitted = word.split
+    word_splitted.each do |w|
+        if (w == word_splitted.first) || ((w!= "and") and (w!="over") and (w!="the"))
+            w.capitalize!
+        end
+    end
+    word_splitted.join(" ")
+end
