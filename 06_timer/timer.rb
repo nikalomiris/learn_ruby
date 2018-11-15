a94308c2aab161986d2b34b8d32986e00e80e759
Kalomiris Nikos <nikalomiris@gmail.com>
2018-11-15 18:15:55 +0200

    all tests pass

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+    def initialize
+      @seconds = 0 
+    end
+
+    def time_string
+      def pad digit
+        digit <10 ? '0' + digit.to_s : digit.to_s
+      end
+    "#{pad(@seconds/3600)}:#{pad(@seconds/60 -(@seconds/3600)*60)}:#{pad(@seconds%60)}"
+    end
+        
 end
